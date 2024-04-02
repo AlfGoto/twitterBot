49d8eabbae39fab2601eb8030ea19bc2441a2ced
Alf <alfgoto@gmail.com>
2024-04-02 13:49:52 +0200

    tweet posting is possible

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var configJS = require('./config.js');
 var conf = new configJS
-// var Bot = require('./bot.js');
-// var Twitter = new Bot(configJS)
-// Twitter.api('https://dev.twitter.com/docs/api/1/post/statuses/update.json')
 
 
 const { TwitterApi } = require("twitter-api-v2");
@@ -16,7 +13,7 @@ const client = new TwitterApi({
 const rwClient = client.readWrite;
 const textTweet = async () => {
     try {
-      await rwClient.v2.tweet("This tweet has been created using nodejs");
+      await rwClient.v2.tweet({text:"C'est quand même plus simple de tweeter sur mon tel que via node.js mais bon"});
       console.log("success");
     } catch (error) {
       console.error(error);
