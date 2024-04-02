99569608df8d8476c520b8dabc52451dae729d34
Alf <alfgoto@gmail.com>
2024-04-02 11:27:40 +0200

    init done

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    consumer_key: 'q4lUyevq5brBn9SokJtTdN5DG',
+    consumer_secret: 'I5bTjR6OdDah4yGFIhOsiA6Z5TyDjOShS86Etkv4UvHqHjGaYP',
+    access_token: '1775088490914443264-7H9VjAnVHI5RYMH3qAvDgXJGcij5ez',
+    access_token_secret: 'PSDc57SsqyX51gIqxyvYc5t1CBcUBL6jsiAFMHG7indkn'
+}
+
+
+//api key : q4lUyevq5brBn9SokJtTdN5DG
+//api key secret : I5bTjR6OdDah4yGFIhOsiA6Z5TyDjOShS86Etkv4UvHqHjGaYP
+//bearer token : AAAAAAAAAAAAAAAAAAAAAIV5tAEAAAAAHt%2B67M5c%2B5177dTPwj9GSTqk7SA%3DOCcYRPqeNdUCl5cafVUu0JvwSvXeveStBQFVYCKyn8PZC7d6eS
+//access token : 1775088490914443264-7H9VjAnVHI5RYMH3qAvDgXJGcij5ez
+//and secret : PSDc57SsqyX51gIqxyvYc5t1CBcUBL6jsiAFMHG7indkn
